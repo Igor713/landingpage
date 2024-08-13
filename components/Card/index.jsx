db9c7cb1b1ffa6357781cca75da9b69485f8bf68
Igor713 <igormatheussiquieri@gmail.com>
2024-08-12 22:50:29 -0300

    development card component

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,12 +1,45 @@
+import Image from 'next';
 import Styles from './card.module.scss';
+import Logo from '../../public/images/logo.png';
 
-const Card = ({ title, typeOf }) => {
+const Card = () => {
+  const cardsData = [
+    {
+      title: 'Card 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur facilis id, ut praesentium veniam.',
+      image: Logo,
+    },
+    {
+      title: 'Card 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur facilis id, ut praesentium veniam.',
+      image: Logo,
+    },
+    {
+      title: 'Card 3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur facilis id, ut praesentium veniam.',
+      image: Logo,
+    },
+    {
+      title: 'Card 4',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur facilis id, ut praesentium veniam.',
+      image: Logo,
+    },
+  ];
+
   return (
-    <button
-      className={`${Styles.button} ${typeOf === 'primary' ? Styles.primary : Styles.secondary}`}
-    >
-      {title}
-    </button>
+    <div className={Styles.cardContainer}>
+      {cardsData.map((card, index) => (
+        <div key={index} className={Styles.card}>
+          <Image src={card.image} alt="Icone" />
+          <h1>{card.title}</h1>
+          <p>{card.description}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
