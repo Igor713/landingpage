b3ea47e34f575ff3fb2f86d656a95040ee7262c1
Igor713 <igormatheussiquieri@gmail.com>
2024-09-05 22:14:05 -0300

    adjust menu mobile

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import Button from '../Button';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '../../public/images/logo.svg';
 import Styles from './header.module.scss';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={Styles.container}>
       <div className={Styles.brand}>
         <Image src={Logo} alt="Logomarca" />
       </div>
-      <div className={Styles.menu}>
+
+      <div className={Styles.hamburgerIcon} onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      <div className={`${Styles.menu} ${isMenuOpen ? Styles.menuOpen : ''}`}>
         <ul>
           <li>
             <Link href="">Home</Link>
@@ -22,7 +35,12 @@ const Header = () => {
             <Link href="">Cases</Link>
           </li>
         </ul>
+
+        <div className={`${Styles.action} ${Styles.mobile}`}>
+          <Button title="Fale conosco" typeOf="primary" />
+        </div>
       </div>
+
       <div className={Styles.action}>
         <Button title="Fale conosco" typeOf="primary" />
       </div>
